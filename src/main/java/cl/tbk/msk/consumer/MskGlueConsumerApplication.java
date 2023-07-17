eee1996325415f00cb1306a594eaa1106b1fff17
Fernando Muñoz <fernando1989mg@gmail.com>
2023-07-17 13:00:34 -0400

    healthcheck

--- src/main/java/cl/tbk/msk/consumer/MskGlueConsumerApplication.java
+++ src/main/java/cl/tbk/msk/consumer/MskGlueConsumerApplication.java
@@ -2,12 +2,20 @@ package cl.tbk.msk.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MskGlueConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MskGlueConsumerApplication.class, args);
 	}
+	
+    @GetMapping("/health")
+    public String getMessages() {
+        return "200!";
+    }
 
 }
