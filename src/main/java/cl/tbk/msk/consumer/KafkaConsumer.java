640f3a40001cfba437f7ae19378981e06f85223e
Fernando Muñoz <fernando1989mg@gmail.com>
2023-12-26 09:08:28 -0300

    metadata

--- src/main/java/cl/tbk/msk/consumer/KafkaConsumer.java
+++ src/main/java/cl/tbk/msk/consumer/KafkaConsumer.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.apache.avro.Schema;
+
 
 @Service
 @PropertySource("classpath:kafka.properties")
@@ -24,5 +26,18 @@ public class KafkaConsumer {
         String message = record.toString();
         System.out.println("Consumed message: " + message);
         //repository.save(new Message(message));
+        
+        Schema schema = record.getSchema();
+
+        // Imprimir el ID del esquema si está disponible
+        // Nota: Esta parte depende de cómo se maneja el registro de esquemas en tu sistema
+        String schemaId = schema.getProp("schemaId"); // Asumiendo que el ID del esquema se almacena en esta propiedad
+        if (schemaId != null) {
+            System.out.println("Schema ID: " + schemaId);
+        } else {
+            System.out.println("Schema ID not found");
+        }
+        
+        System.out.println("Schema Fields: " + schema.getFields());
     }
 }
